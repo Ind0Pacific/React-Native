ab26a1be05aa814d4ab6a00e3c2558184f0b4f44
Ind0Pacific <vashishtdeepanshu87@gmail.com>
2026-09-19 23:51:31 +0530

    Added style

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style = {styles.dummyText}>
+          Open up App.js to start working on your app!
+          </Text>
+      </View>
+      <Text style = {styles.dummyText}> 
+        hey
+        </Text >
+      <Button title="HEY" onPress={() => console.log("hey")} />
     </View>
   );
 }
@@ -13,8 +19,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  dummyText: {
+    margin: 16,
+    borderWidth: 10,
+    borderColor: "red",
+    padding: 10,
   },
 });
